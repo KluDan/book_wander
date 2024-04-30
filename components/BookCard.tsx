398244da17677c5353021d5bdd9adc56d5ef51e0
Danylo Kliushnychenko <danylktv2@gmail.com>
2024-04-30 12:04:43 +0100

    feat: Add dialog component for book card

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,5 +1,7 @@
 import { Book } from "@/lib/types";
 import Image from "next/image";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import Button from "./CustomButton";
 
 type BookCardProps = {
   book: Book;
@@ -7,46 +9,49 @@ type BookCardProps = {
 
 const BookCard: React.FC<BookCardProps> = ({ book }) => {
   return (
-    /*   <Dialog>
-      <DialogTrigger asChild> */
-    <article className="flex flex-col gap-[8px] h-full">
-      <div className="h-[84%]">
-        <Image
-          src={book.imageUrl}
-          alt="Book"
-          width={137}
-          height={208}
-          className="rounded-[8px] w-full h-full"
-        />
-      </div>
-      <h2 className="font-[700] text-[14px] leading-[129%] tracking-[-0.02em] truncate">
-        {book.title}
-      </h2>
-      <h3 className="text-[10px] text-textSecondary leading-[120%] tracking-[-0.02em]">
-        {book.author}
-      </h3>
-    </article>
-    /*  </DialogTrigger> */
-    /*       <DialogContent className="w-full md:w-[500px] py-[40px] md:py-[50px] bg-bgPrimary border-componentStroke rounded-[12px] flex flex-col ">
-        <div className="max-h-[233px] max-w-[153px] mb-[16px]">
+    <Dialog>
+      <DialogTrigger asChild>
+        <article className="flex flex-col justify-between h-full xl:h-[248px] cursor-pointer">
+          <div className="h-[84%] hover:scale-[102%] transition">
+            <Image
+              src={book.imageUrl}
+              alt="Book"
+              width={137}
+              height={208}
+              className="rounded-[8px] w-full h-full"
+            />
+          </div>
+
+          <h2 className="font-[700] text-[14px] leading-[129%] tracking-[-0.02em] truncate">
+            {book.title}
+          </h2>
+          <h3 className="text-[10px] text-textSecondary leading-[120%] tracking-[-0.02em]">
+            {book.author}
+          </h3>
+        </article>
+      </DialogTrigger>
+      <DialogContent className="w-[335px] flex-center gap-0 flex-col md:w-[500px] py-[40px] md:py-[50px] bg-bgPrimary border-componentStroke rounded-[12px]">
+        <div className="mb-[16px]">
           <Image
             src={book.imageUrl}
             alt="Book"
             width={153}
             height={233}
-            className="rounded-[8px]"
+            className="rounded-[8px] h-[233px]"
           />
         </div>
-        <h2 className="font-[700] text-[14px] leading-[129%] tracking-[-0.02em] truncate">
+        <h2 className="font-[700] text-[16px] leading-[100%] tracking-[-0.02em] max-w-[200px] text-center mb-[2px] xl:text-[18px] xl:max-w-[300px]">
           {book.title}
         </h2>
-        <h3 className="text-[10px] text-textSecondary leading-[120%] tracking-[-0.02em]">
+        <h3 className="text-[12px] text-textSecondary leading-[117%] tracking-[-0.02em] mb-[4px] xl:text-[16px]">
           {book.author}
         </h3>
-        <p>{book.totalPages}</p>
+        <span className="text-[10px] leading-[120%] tracking-[-0.02em] text-center mb-[20px] md:mb-[32px] xl:text-[14px]">
+          {book.totalPages} pages
+        </span>
         <Button>Add to library</Button>
       </DialogContent>
-    </Dialog> */
+    </Dialog>
   );
 };
 
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -22,14 +22,14 @@ const Button = ({ children, className, variant, ...props }: ButtonProps) => {
 export default Button;
 
 const buttonVariants = cva(
-  "inline-block border border-transparent rounded-[30px] text-[14px] leading-[129%] tracking-[0.02em] font-[700] md:text-[16px] md:leading-[112%] transition",
+  "inline-block border border-transparent rounded-[30px] text-[14px] leading-[129%] tracking-[0.02em] font-[700] md:text-[16px] md:leading-[112%] transition focus:outline-none",
   {
     variants: {
       variant: {
         filled:
-          "py-[12px] px-[45px] md:py-[16px] md:px-[54px] bg-textPrimary text-bgPrimary hover:bg-transparent hover:text-textPrimary hover:border-componentStroke md:text-[20px] md:leading-[100%]",
+          "py-[12px] px-[45px] md:py-[16px] md:px-[54px] bg-textPrimary text-bgPrimary hover:text-textPrimary hover:bg-transparent hover:border-componentStroke md:text-[20px] md:leading-[100%] focus:text-textPrimary focus:bg-transparent focus:border-componentStroke",
         outlined:
-          "px-[20px] py-[10px] md:py-[12px] md:px-[28px] border-componentStroke hover:border-transparent bg-transparent text-textPrimary hover:bg-textPrimary hover:text-bgPrimary",
+          "px-[20px] py-[10px] md:py-[12px] md:px-[28px] border-componentStroke bg-transparent text-textPrimary hover:border-transparent hover:bg-textPrimary hover:text-bgPrimary focus:border-transparent focus:bg-textPrimary focus:text-bgPrimary",
       },
     },
     defaultVariants: {
